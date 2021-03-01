7166b91690d1b834919798b5d0956e0877dcb23d
Luca Pasquale <lucapasquale.094@gmail.com>
2021-02-28 23:32:40 -0300

    fixed config env_vars

--- config/config.exs
+++ config/config.exs
@@ -9,12 +9,12 @@ config :botchini, Botchini.Repo,
 
 config :botchini,
   port: System.get_env("PORT") || 3000,
-  host: System.get_env("HOST") || "https://780fa33c1f52.ngrok.io",
+  host: System.get_env("HOST", "https://780fa33c1f52.ngrok.io"),
   ecto_repos: [Botchini.Repo],
   twitch_url: "https://api.twitch.tv/helix",
   twitch_headers: [
     {"client-id", System.get_env("TWITCH_CLIENT_ID")},
-    {"authorization", "Bearer " <> System.get_env("TWITCH_TOKEN")}
+    {"authorization", "Bearer " <> System.get_env("TWITCH_TOKEN", "")}
   ]
 
 config :nostrum,

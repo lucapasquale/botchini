fbff091a9b9496b272705d5b2129ccc527eaeae8
Luca Pasquale <lucapasquale.094@gmail.com>
2021-03-02 14:11:13 -0300

    mix deploy changes

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Botchini.MixProject do
       app: :botchini,
       version: "0.1.0",
       elixir: "~> 1.11",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

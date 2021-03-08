ed28f93a1cbd5b78bcc4a006707881117ad8f8ed
Luca Pasquale <lucapasquale.094@gmail.com>
2021-03-07 21:10:14 -0300

    added pr-check workflow

--- mix.exs
+++ mix.exs
@@ -4,12 +4,13 @@ defmodule Botchini.MixProject do
   def project do
     [
       app: :botchini,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.11",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -32,11 +33,20 @@ defmodule Botchini.MixProject do
       {:hackney, "~> 1.17.0"},
       {:jason, ">= 1.0.0"},
       # HTTP Server
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      # Local
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
   defp aliases do
     [test: ["ecto.create --quiet", "ecto.migrate", "test"]]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+    ]
   end
 end

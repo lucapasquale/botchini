238306003ee9c9bdef0aaa189a4f18804cd4baf0
Luca Pasquale <lucapasquale.094@gmail.com>
2021-04-05 17:20:14 -0300

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Botchini.MixProject do
   def project do
     [
       app: :botchini,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.11",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,

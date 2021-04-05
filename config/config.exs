f925a2e869e0ba81959809fdaa84133609ea21d7
Luca Pasquale <lucapasquale.094@gmail.com>
2021-04-04 23:38:11 -0300

    config

--- config/config.exs
+++ config/config.exs
@@ -3,17 +3,7 @@ import Config
 config :botchini,
   environment: Mix.env(),
   ecto_repos: [Botchini.Repo],
-  host: System.get_env("HOST"),
-  port: System.get_env("PORT", "3010") |> String.to_integer(),
-  twitch_client_id: System.get_env("TWITCH_CLIENT_ID"),
-  twitch_client_secret: System.get_env("TWITCH_CLIENT_SECRET")
-
-config :botchini, Botchini.Repo,
-  database: "botchini",
-  url: System.get_env("POSTGRES_URL")
-
-config :nostrum,
-  token: System.get_env("DISCORD_TOKEN")
+  port: System.get_env("PORT", "3010") |> String.to_integer()
 
 config :porcelain,
   driver: Porcelain.Driver.Basic

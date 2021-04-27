4d548227b4f00edad06cb0c49bc8d020beef15af
Luca Pasquale <lucapasquale.094@gmail.com>
2021-04-26 23:29:45 -0300

    using Task to send message, added logs (#19)
    * using Task to send message, added logs
    
    * separated into functions

--- lib/botchini/routes/twitch.ex
+++ lib/botchini/routes/twitch.ex
@@ -25,7 +25,7 @@ defmodule Botchini.Routes.Twitch do
             %{status: 404, body: "Invalid stream"}
 
           stream ->
-            send_followers_message(stream)
+            send_stream_online_messages(stream)
             %{status: 200, body: "OK"}
         end
     end
@@ -48,21 +48,40 @@ defmodule Botchini.Routes.Twitch do
     end
   end
 
-  defp send_followers_message(stream) do
+  defp send_stream_online_messages(stream) do
     user_data = API.get_user(stream.code)
     stream_data = API.get_stream(stream.code)
 
-    Enum.each(StreamFollower.find_all_for_stream(stream.id), fn follower ->
-      channel_id = Map.get(follower, :discord_channel_id)
+    followers = StreamFollower.find_all_for_stream(stream.id)
 
-      case StreamOnline.send_message(String.to_integer(channel_id), {user_data, stream_data}) do
-        {:error, _err} ->
-          Logger.warn("Removing channel since doesn't exist anymore", channel_id: channel_id)
-          Domain.Stream.stop_following(stream.code, channel_id)
+    Logger.info("Stream #{stream.code} is online, sending to #{length(followers)} channels")
 
-        _ ->
-          :noop
-      end
+    Enum.each(followers, fn follower ->
+      Task.start(fn ->
+        send_followers_message(stream, follower, {user_data, stream_data})
+      end)
     end)
+  end
+
+  defp send_followers_message(stream, follower, {user_data, stream_data}) do
+    channel_id = Map.get(follower, :discord_channel_id)
+
+    msg_response =
+      StreamOnline.send_message(
+        String.to_integer(channel_id),
+        {user_data, stream_data}
+      )
+
+    case msg_response do
+      {:error, _err} ->
+        Logger.warn("Removing channel since doesn't exist anymore",
+          channel_id: channel_id
+        )
+
+        Domain.Stream.stop_following(stream.code, channel_id)
+
+      _ ->
+        :noop
+    end
   end
 end

85f447c28e17baf0ce9afe07b367231b0dae8d79
Luca Pasquale <lucapasquale.094@gmail.com>
2021-05-07 14:08:43 -0300

    added tests for discord context, fixed timestamp format on embeds

---
+++ test/botchini/discord/discord_test.exs
@@ -0,0 +1,27 @@
+defmodule BotchiniTest.Discord.DiscordTest do
+  use Botchini.DataCase, async: false
+
+  import Mock
+
+  alias Botchini.{Discord, Repo}
+  alias Botchini.Discord.Schema.Guild
+
+  describe "upsert_guild" do
+    test "create new guild if doesnt exist" do
+      discord_guild_id = Faker.String.base64()
+
+      {:ok, guild} = Discord.upsert_guild(discord_guild_id)
+
+      assert guild != nil
+      assert guild.discord_guild_id == discord_guild_id
+    end
+
+    test "use existing stream" do
+      existing_guild = generate_guild()
+
+      {:ok, guild} = Discord.upsert_guild(existing_guild.discord_guild_id)
+
+      assert guild == existing_guild
+    end
+  end
+end

f5a29c0e4b385699ff7409de99589d53fa6bb180
Luca Pasquale <lucapasquale.094@gmail.com>
2021-09-26 19:19:24 -0300

    using stable nostrum release

--- mix.exs
+++ mix.exs
@@ -24,8 +24,7 @@ defmodule Botchini.MixProject do
   defp deps do
     [
       # Discord
-      {:nostrum, github: "Kraigie/nostrum", runtime: Mix.env() != :test},
-      {:gun, "== 2.0.0-rc.2", override: true},
+      {:nostrum, "~> 0.4.6", runtime: Mix.env() != :test},
       # Ecto
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
@@ -35,7 +34,6 @@ defmodule Botchini.MixProject do
       {:jason, ">= 1.0.0"},
       {:exconstructor, "~> 1.1.0"},
       # HTTP Server
-      {:cowlib, ">= 2.11.0", override: true},
       {:plug_cowboy, "~> 2.0"},
       # Logging
       {:ink, "~> 1.0"},

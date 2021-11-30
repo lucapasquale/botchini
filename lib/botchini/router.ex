9f29ef599236a8788b2cc29f19228bdb8042f81b
Luca Pasquale <lucapasquale.094@gmail.com>
2021-11-30 00:01:44 -0300

    checkpoint

--- lib/botchini/router.ex
+++ lib/botchini/router.ex
@@ -18,19 +18,25 @@ defmodule Botchini.Router do
   end
 
   get "/custom" do
-    all_streams = from(s in Stream) |> Repo.stream()
+    update_streams()
 
-    Repo.transaction(fn ->
-      Enum.to_list(all_streams)
+    send_resp(conn, 200, "ok")
+  end
+
+  defp update_streams(offset \\ 0) do
+    streams =
+      from(s in Stream, limit: 20, offset: ^offset, order_by: s.id)
+      |> Repo.all()
       |> Enum.map(fn stream ->
         user = Botchini.Twitch.API.get_user(stream.code)
 
         Stream.changeset(stream, %{name: user.display_name})
         |> Repo.update!()
       end)
-    end)
 
-    send_resp(conn, 200, "ok")
+    if length(streams) == 20 do
+      update_streams(offset + 20)
+    end
   end
 
   post "/twitch/webhooks/callback" do

b332d9d166e3e266289396d72b13260df54a4c6c
Luca Pasquale <lucapasquale.094@gmail.com>
2022-07-16 00:57:52 -0300

    checkpoint

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Botchini.MixProject do
   def project do
     [
       app: :botchini,
-      version: "5.1.1",
+      version: "5.2.0",
       elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,

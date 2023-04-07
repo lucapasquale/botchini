c5bd3f1bcb1e28a081dc15c2111b449027df2b08
Luca Pasquale <lucapasquale.094@gmail.com>
2023-04-07 17:41:40 -0300

    pause/skip/resume/stop commands

--- lib/botchini_discord/music/interactions/music.ex
+++ lib/botchini_discord/music/interactions/music.ex
@@ -31,6 +31,26 @@ defmodule BotchiniDiscord.Music.Interactions.Music do
               description: "YouTube URL or search term"
             }
           ]
+        },
+        %{
+          name: "pause",
+          description: "Pause current song",
+          type: 1
+        },
+        %{
+          name: "resume",
+          description: "Resume current song",
+          type: 1
+        },
+        %{
+          name: "skip",
+          description: "Skip current song",
+          type: 1
+        },
+        %{
+          name: "stop",
+          description: "Stop playing",
+          type: 1
         }
       ]
     }
@@ -48,6 +68,18 @@ defmodule BotchiniDiscord.Music.Interactions.Music do
     cond do
       Helpers.get_option(options, "play") ->
         handle_play(interaction, options)
+
+      Helpers.get_option(options, "pause") ->
+        handle_pause(interaction, options)
+
+      Helpers.get_option(options, "resume") ->
+        handle_resume(interaction, options)
+
+      Helpers.get_option(options, "skip") ->
+        handle_skip(interaction, options)
+
+      Helpers.get_option(options, "stop") ->
+        handle_stop(interaction, options)
 
       true ->
         %{
@@ -78,10 +110,101 @@ defmodule BotchiniDiscord.Music.Interactions.Music do
         %{
           type: 4,
           data: %{
-            content: "Added #{url} to queue",
-            components: [Components.pause_controls()]
+            content: "Added #{url} to queue"
+            # components: [Components.pause_controls()]
           }
         }
+    end
+  end
+
+  defp handle_pause(interaction, _options) do
+    guild = Discord.fetch_guild(Integer.to_string(interaction.guild_id))
+
+    if Nostrum.Voice.playing?(interaction.guild_id) do
+      Music.pause(guild)
+      Nostrum.Voice.pause(interaction.guild_id)
+
+      %{
+        type: 4,
+        data: %{
+          content: "Paused current song"
+          # components: [Components.resume_controls()]
+        }
+      }
+    else
+      %{
+        type: 4,
+        data: %{content: "Not currently playing"}
+      }
+    end
+  end
+
+  defp handle_resume(interaction, _options) do
+    guild = Discord.fetch_guild(Integer.to_string(interaction.guild_id))
+    cur_track = Music.get_current_track(guild)
+
+    if is_nil(cur_track) || cur_track.status != :paused do
+      %{
+        type: 4,
+        data: %{content: "No song to resume!"}
+      }
+    else
+      Music.resume(guild)
+      :ok = Nostrum.Voice.resume(interaction.guild_id)
+
+      %{
+        type: 4,
+        data: %{
+          content: "Resuming current song"
+          # components: [Components.pause_controls()]
+        }
+      }
+    end
+  end
+
+  defp handle_stop(interaction, _options) do
+    guild = Discord.fetch_guild(Integer.to_string(interaction.guild_id))
+
+    case Music.get_current_track(guild) do
+      nil ->
+        %{
+          type: 4,
+          data: %{content: "No song in queue"}
+        }
+
+      _track ->
+        Music.clear_queue(guild)
+        Nostrum.Voice.stop(interaction.guild_id)
+
+        %{
+          type: 4,
+          data: %{content: "Stopped playing"}
+        }
+    end
+  end
+
+  defp handle_skip(interaction, _options) do
+    guild = Discord.fetch_guild(Integer.to_string(interaction.guild_id))
+    next_song = Music.get_next_track(guild)
+
+    if is_nil(next_song) do
+      Music.clear_queue(guild)
+      :ok = Nostrum.Voice.stop(interaction.guild_id)
+
+      %{
+        type: 4,
+        data: %{content: "No song in queue, stopping"}
+      }
+    else
+      :ok = Nostrum.Voice.stop(interaction.guild_id)
+
+      %{
+        type: 4,
+        data: %{
+          content: "Skipping to next song"
+          # components: [Components.pause_controls()]
+        }
+      }
     end
   end
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Botchini.MixProject do
   def project do
     [
       app: :botchini,
-      version: "8.0.1",
+      version: "8.1.0",
       elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,

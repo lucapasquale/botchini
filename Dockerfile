acc1b10d0fc41161573617e721bce865f5838754
Luca Pasquale <lucapasquale.094@gmail.com>
2023-06-19 20:23:02 -0300

    testing new elixir version

--- Dockerfile
+++ Dockerfile
@@ -12,9 +12,9 @@
 #   - https://pkgs.org/ - resource for finding needed packages
 #   - Ex: hexpm/elixir:1.12.3-erlang-24.1.2-debian-bullseye-20210902-slim
 #
-ARG ELIXIR_VERSION=1.12.3
-ARG OTP_VERSION=24.1.2
-ARG DEBIAN_VERSION=bullseye-20210902-slim
+ARG ELIXIR_VERSION=1.15.0
+ARG OTP_VERSION=26.0.1
+ARG DEBIAN_VERSION=bullseye-20230522-slim
 
 ARG BUILDER_IMAGE="hexpm/elixir:${ELIXIR_VERSION}-erlang-${OTP_VERSION}-debian-${DEBIAN_VERSION}"
 ARG RUNNER_IMAGE="debian:${DEBIAN_VERSION}"
--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Botchini.MixProject do
   def project do
     [
       app: :botchini,
-      version: "8.5.0",
-      elixir: "~> 1.12",
+      version: "8.5.1",
+      elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -58,7 +58,6 @@ defmodule Botchini.MixProject do
       {:ink, "~> 1.0"},
       {:quantum, "~> 3.0"},
       # Development and testing
-      {:remix, "~> 0.0.1", only: :dev},
       {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false},
       {:patch, "~> 0.12.0", only: [:test]},
       {:faker, "~> 0.16", only: :test}

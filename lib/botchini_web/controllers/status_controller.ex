f4563f862f82ac03e9b630a5ca3733aa8424c6bf
Luca Pasquale <lucapasquale.094@gmail.com>
2024-02-04 13:55:48 -0300

    removed old web app

--- lib/botchini_web/controllers/status_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule BotchiniWeb.StatusController do
-  use BotchiniWeb, :controller
-
-  def index(conn, _params) do
-    conn |> json(%{ok: true})
-  end
-end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Botchini.MixProject do
   def project do
     [
       app: :botchini,
-      version: "8.9.0",
+      version: "8.9.1",
       elixir: "~> 1.16.1",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
@@ -29,38 +29,22 @@ defmodule Botchini.MixProject do
       # Discord
       {:nostrum, git: "https://github.com/Kraigie/nostrum.git", runtime: Mix.env() != :test},
       {:cowlib, "~> 2.11", hex: :remedy_cowlib, override: true},
-      # Phoenix
-      {:phoenix, "~> 1.6.16"},
-      {:phoenix_html, "~> 3.3.1"},
-      {:phoenix_live_dashboard, "~> 0.6"},
-      {:phoenix_live_reload, "~> 1.4.1", only: :dev},
-      {:phoenix_live_view, "~> 0.17.14"},
-      {:floki, ">= 0.34.2", only: :test},
-      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.22.1"},
-      {:jason, "~> 1.4.0"},
-      {:plug_cowboy, "~> 2.6.1"},
-      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
-      {:petal_components, "~> 0.17"},
-      {:elixir_xml_to_map, "~> 2.0"},
       # Ecto
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.9.2"},
-      {:postgrex, ">= 0.16.5"},
+      {:ecto_sql, "~> 3.11.1"},
+      {:postgrex, ">= 0.17.4"},
       # HTTP Client
-      {:tesla, "~> 1.6.0"},
+      {:tesla, "~> 1.8.0"},
       {:gun, "~> 2.0", override: true},
-      {:hackney, "~> 1.17.0"},
-      {:exconstructor, "~> 1.1.0"},
+      {:hackney, "~> 1.20.1"},
+      {:exconstructor, "~> 1.2.10"},
       # Helpers
       {:ink, "~> 1.0"},
       {:quantum, "~> 3.0"},
       # Development and testing
       {:exsync, "~> 0.2", only: :dev},
       {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false},
-      {:patch, "~> 0.12.0", only: [:test]},
+      {:patch, "~> 0.13.0", only: [:test]},
       {:faker, "~> 0.16", only: :test}
     ]
   end

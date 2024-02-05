b85cf53df7fadddb0353afd3d22cf3142dbd0440
Luca Pasquale <lucapasquale.094@gmail.com>
2024-02-04 21:18:35 -0300

    checkpoint

--- lib/botchini_web/components/core_components.ex
+++ lib/botchini_web/components/core_components.ex
@@ -16,7 +16,7 @@ defmodule BotchiniWeb.CoreComponents do
   """
   use Phoenix.Component
 
-  alias Phoenix.HTML
+  alias Phoenix.HTML.Form
   alias Phoenix.LiveView.JS
 
   import BotchiniWeb.Gettext
@@ -341,7 +341,7 @@ defmodule BotchiniWeb.CoreComponents do
         {@rest}
       >
         <option :if={@prompt} value=""><%= @prompt %></option>
-        <%= Phoenix.HTML.Form.options_for_select(@options, @value) %>
+        <%= Form.options_for_select(@options, @value) %>
       </select>
       <.error :for={msg <- @errors}><%= msg %></.error>
     </div>
@@ -362,7 +362,7 @@ defmodule BotchiniWeb.CoreComponents do
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
         {@rest}
-      ><%= Phoenix.HTML.Form.normalize_value("textarea", @value) %></textarea>
+      ><%= Form.normalize_value("textarea", @value) %></textarea>
       <.error :for={msg <- @errors}><%= msg %></.error>
     </div>
     """
@@ -377,7 +377,7 @@ defmodule BotchiniWeb.CoreComponents do
         type={@type}
         name={@name}
         id={@id}
-        value={Phoenix.HTML.Form.normalize_value(@type, @value)}
+        value={Form.normalize_value(@type, @value)}
         class={[
           "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
           "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
